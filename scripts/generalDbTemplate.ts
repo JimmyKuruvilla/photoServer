7f39f4f660019672783ccda01695b644781fb812
JimmyKuruvilla <jimmyjk@gmail.com>
2025-10-31 14:57:52 -0500

    Add purge by pattern and delete .avi, picasa, .DS_store etc

--- scripts/generalDbTemplate.ts
+++ scripts/generalDbTemplate.ts
@@ -1,28 +1,28 @@
-#!/usr/bin/env node
-import { TABLES } from '../src/constants.ts';
-import { localDb } from '../src/db/initDb.ts';
-import { genFileHash } from './lib/hash.ts';
-import { log } from './lib/log.ts';
+// #!/usr/bin/env node
+// import { unlink } from 'fs/promises';
+// import { COLS, TABLES } from '../../src/constants.ts';
+// import { localDb } from '../../src/db/initDb.ts';
+// import { log } from '../lib/log.ts';
 
-/*
- Template starting point for db scripts
-*/
+// /*
+//  Search db for a file path pattern, and delete those files from disk, and from db
+//  Requires aws sync --delete to remove from remote
+// */
 
-const db = await localDb();
+// const db = await localDb();
 
-(async () => {
-  const recordsStream = await db(TABLES.MEDIA).where('hash', null).stream()
-  
-  for await (const record of recordsStream) {
-    try {
-      const hash = await genFileHash(record.path)
-      const trx = await db.transaction();
-      await trx(TABLES.MEDIA).where('path', record.path).update({ hash });
-      await trx.commit()
-      console.log(hash, record.path)
-    } catch (error: any) {
-      log(`ERROR: ${error.message}`)
-    }
-  }
-  process.exit();
-})();
+// (async () => {
+//   const recordsStream = await db(TABLES.MEDIA).whereILike(COLS.MEDIA.PATH, '%.avi').stream()
+
+//   for await (const record of recordsStream) {
+//     try {
+//       await unlink(record.path)
+//       await db(TABLES.MEDIA).where('id', record.id).delete()
+//       console.log(record.path);
+//     } catch (error: any) {
+//       log(`ERROR: ${error.message}`)
+//     }
+//   }
+//   process.exit();
+// })();
+// // 
---
+++ scripts/purgeFilesByPattern/purgeFilesByPattern.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import { unlink } from 'fs/promises';
+import { COLS, TABLES } from '../../src/constants.ts';
+import { localDb } from '../../src/db/initDb.ts';
+import { log } from '../lib/log.ts';
+
+/*
+ Search db for a file path pattern, and delete those files from disk, and from db
+ Requires aws sync --delete to remove from remote
+*/
+
+const db = await localDb();
+
+(async () => {
+  const recordsStream = await db(TABLES.MEDIA).whereILike(COLS.MEDIA.PATH, '%.avi').stream()
+
+  for await (const record of recordsStream) {
+    try {
+      await unlink(record.path)
+      await db(TABLES.MEDIA).where('id', record.id).delete()
+      console.log(record.path);
+    } catch (error: any) {
+      log(`ERROR: ${error.message}`)
+    }
+  }
+  process.exit();
+})();
