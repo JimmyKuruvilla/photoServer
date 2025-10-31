7db44bc7f42c42f26ef09c30b133ad96c4554d26
JimmyKuruvilla <jimmyjk@gmail.com>
2025-10-31 08:46:23 -0500

    Add deleted table

---
+++ src/db/migrations/20251031132946_addDeletedTable.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function (knex) {
+  return knex.raw(`
+   CREATE TABLE IF NOT EXISTS deleted
+    (
+      id SERIAL PRIMARY KEY,
+      hash TEXT NOT NULL,
+      path TEXT NOT NULL,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+    );
+
+  CREATE INDEX deleted_hash_idx on deleted (hash);
+    `
+  );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function (knex) {
+  return knex.raw(`
+     DROP TABLE IF EXISTS deleted;
+  `)
+};
